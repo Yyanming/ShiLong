1f9dc804cb32c2abc533311c4a04691b34a364b3
Yyanming <84701816+Yyanming@users.noreply.github.com>
2021-07-31 00:09:38 +0800

    Update webpack.config.js

--- task-4/webpack.config.js
+++ task-4/webpack.config.js
@@ -1,1 +1,10 @@
+const path = require('path');
 
+module.exports = {
+    mode :'development',
+    entry :'./mydev/src/index',
+    output :{
+        path: path.resolve( __dirname,"dist"),
+        filename :'xxfn.js'
+    }
+}
